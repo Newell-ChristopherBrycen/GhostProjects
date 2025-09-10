229cc83487fb484acd70669e0c0089c32c7b0d51
Brycen Newell <brynewell@gmail.com>
2025-09-10 14:44:00 -0500

    trying something different

--- scripts/encode-image.sh
+++ scripts/encode-image.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
+
+cat Applications/homepage/homepage-configmap.yaml > scripts/homepage-configmap.yaml
+
+
 # This script encodes the pictures for the homepage config map
-echo "apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: pictures
-  namespace: homepage
-  labels:
-    app.kubernetes.io/name: pictures
-data:" > Applications/homepage/homepage-pictures.yaml
+
 
 
 for file in Content/Backgrounds/*
@@ -16,8 +13,8 @@ do
   filename_ext=$(basename "$file")
   filename=${filename_ext%.*}
   file64=$(base64 "$file" | sed 's/^/    /')
-  echo "  $filename: |" >> Applications/homepage/homepage-pictures.yaml
-  echo "$file64"  >> Applications/homepage/homepage-pictures.yaml
+  echo "  $filename: |" >> scripts/homepage-configmap.yaml
+  echo "$file64"  >> scripts/homepage-configmap.yaml
 done
 
 for file in Content/icons/*
@@ -25,6 +22,6 @@ do
   filename_ext=$(basename "$file")
   filename=${filename_ext%.*}
   file64=$(base64 "$file" | sed 's/^/    /')
-  echo "  $filename: |" >> Applications/homepage/homepage-pictures.yaml
-  echo "$file64"  >> Applications/homepage/homepage-pictures.yaml
+  echo "  $filename: |" >> scripts/homepage-configmap.yaml
+  echo "$file64"  >> scripts/homepage-configmap.yaml
 done
