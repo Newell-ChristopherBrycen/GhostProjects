ebe0c9b22efca6b67f6785ec2805b889ebcc3b41
Brycen Newell <brynewell@gmail.com>
2025-09-10 13:37:27 -0500

    ConfigMap Pictures

---
+++ scripts/encode-image.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This script encodes the pictures for the homepage config map
+echo "apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: pictures
+  namespace: homepage
+  labels:
+    app.kubernetes.io/name: pictures
+data:" > ../Applications/homepage/homepage-pictures.yaml
+
+
+for file in /Content/Backgrounds/*
+do
+  filename=$(basename "$file")
+  file64=$(base64 $file)
+  echo "$filename: $file64" >> ../Applications/homepage/homepage-pictures.yaml
+done
+
+for file in /Content/Icons/*
+do
+  filename=$(basename "$file")
+  file64=$(base64 $file)
+  echo "$filename: $file64" >> ../Applications/homepage/homepage-pictures.yaml
+done
