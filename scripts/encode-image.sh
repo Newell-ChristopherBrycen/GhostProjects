87ebeb82ce35ef7514e9ff52fcb73b692c222541
Brycen Newell <brynewell@gmail.com>
2025-09-10 13:52:40 -0500

    Updated script and images output

--- scripts/encode-image.sh
+++ scripts/encode-image.sh
@@ -14,13 +14,15 @@ data:" > Applications/homepage/homepage-pictures.yaml
 for file in Content/Backgrounds/*
 do
   filename=$(basename "$file")
+  filename_no_ext=${filename%.*}
   file64=$(base64 $file)
-  echo "$filename: $file64" >> Applications/homepage/homepage-pictures.yaml
+  echo "$filename_no_ext: $file64" >> Applications/homepage/homepage-pictures.yaml
 done
 
 for file in Content/icons/*
 do
   filename=$(basename "$file")
+  filename_no_ext=${filename%.*}
   file64=$(base64 $file)
-  echo "$filename: $file64" >> Applications/homepage/homepage-pictures.yaml
+  echo "$filename_no_ext: $file64" >> Applications/homepage/homepage-pictures.yaml
 done
