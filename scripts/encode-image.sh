532b593d65ec15773dff3c069c1349fbc1efd1a5
Brycen Newell <brynewell@gmail.com>
2025-09-18 14:12:30 -0500

    going to push images via CI

--- scripts/encode-image.sh
+++ scripts/encode-image.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-
-cat Applications/homepage/homepage-configmap.yaml > scripts/homepage-configmap.yaml
-
-
-# This script encodes the pictures for the homepage config map
-
-
-
 for file in Content/Backgrounds/*
 do
   filename_ext=$(basename "$file")
   filename=${filename_ext%.*}
   file64=$(base64 "$file" | sed 's/^/    /')
-  echo "  $filename: |" >> scripts/homepage-configmap.yaml
-  echo "$file64"  >> scripts/homepage-configmap.yaml
+  echo "  $filename: |" >> scripts/result.txt
+  echo "$file64"  >> scripts/result.txt
 done
 
 for file in Content/icons/*
@@ -22,6 +14,6 @@ do
   filename_ext=$(basename "$file")
   filename=${filename_ext%.*}
   file64=$(base64 "$file" | sed 's/^/    /')
-  echo "  $filename: |" >> scripts/homepage-configmap.yaml
-  echo "$file64"  >> scripts/homepage-configmap.yaml
+  echo "  $filename: |" >> scripts/result.txt
+  echo "$file64"  >> scripts/result.txt
 done
